a70eed344d42ebdbe33f06e71a0d024742c64c60
bivascu <ivascu.bogdan.ca@gmail.com>
2015-05-30 13:42:18 -0400

    Adding model

---
+++ src/java/com/portefeuille/model/Company.java
@@ -0,0 +1,36 @@
+package com.portefeuille.model;
+
+import java.math.BigDecimal;
+
+public class Company {
+	
+	private String name;
+	private String ticker;
+	private BigDecimal buyPrice;
+	private BigDecimal marketPrice;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTicker() {
+		return ticker;
+	}
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+	public BigDecimal getBuyPrice() {
+		return buyPrice;
+	}
+	public void setBuyPrice(BigDecimal buyPrice) {
+		this.buyPrice = buyPrice;
+	}
+	public BigDecimal getMarketPrice() {
+		return marketPrice;
+	}
+	public void setMarketPrice(BigDecimal marketPrice) {
+		this.marketPrice = marketPrice;
+	}
+}
